5bda255d7a581c49f44b566f38ea989fef66dae0
Dimildizio <42382713+Dimildizio@users.noreply.github.com>
2024-04-20 04:24:07 +0800

    feat(swap): add fastapi face swap interface

--- src/main.py
+++ src/main.py
@@ -1,6 +1,18 @@
-from generation.chargen import generate_image
+import asyncio
+import os
 
+from constants import FOLDER
+from generation.chargen import generate_image
+from swapper.swap_requests import send_image_to_swapper
+
+
+async def main():
+    character = {'race': 'dwarf', 'dnd_class': 'thief', 'background': 'dungeon', 'hair': 'red', 'beard': True}
+
+    source_img = os.path.join(FOLDER, 'img_735850.png')  # test
+    target_img = generate_image(character)
+    face_path = await send_image_to_swapper(source_img, target_img)
+    print(face_path)
 
 if __name__ == '__main__':
-    character = {'race': 'dwarf', 'dnd_class': 'wizard', 'background': 'tavern'}
-    generate_image(character)
+    asyncio.run(main())
