448433b3453dff0e30fc29f64a6177792d5490cf
Dimildizio <42382713+Dimildizio@users.noreply.github.com>
2024-04-22 22:47:46 +0800

    fix(db): revert to one class orm using current imags, no history

--- src/database/user_db.py
+++ src/database/user_db.py
@@ -1,5 +1,6 @@
-from sqlalchemy import select, Column, Integer, String, Boolean, ForeignKey
-from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+from datetime import datetime
+from sqlalchemy import select, Column, Integer, String, Boolean, ForeignKey, DateTime
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship, joinedload
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -10,37 +11,6 @@ from src.constants import ASYNC_DB_URL
 Base = declarative_base()
 async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
 async_session = sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
-
-
-
-
-class Character(Base):
-    __tablename__ = 'characters'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    race = Column(String(50), default='dwarf')
-    gender = Column(Boolean, default=False)
-    dnd_class = Column(String(50), default='fighter')
-    hair = Column(String(50), default='black')
-    beard = Column(Boolean, default=True)
-    background = Column(String(255), default='forest')
-
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship("User", back_populates="character", uselist=False)
-
-
-class Triplet(Base):
-    __tablename__ = 'triplets'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    target_image = Column(String(255), nullable=True, default=None)
-    face_image = Column(String(255), nullable=True, default=None)
-    result_image = Column(String(255), nullable=True, default=None)
-
-    character_id = Column(Integer, ForeignKey('characters.id'))
-    character = relationship("Character")
-    user = relationship("User", back_populates="triplets")
 
 
 class User(Base):
@@ -55,10 +25,16 @@ class User(Base):
     is_premium = Column(Boolean, default=False)
     requests_left = Column(Integer, default=10)
 
-    character_id = Column(Integer, ForeignKey('characters.id'))
-    character = relationship("Character", back_populates="user", uselist=False)
+    race = Column(String(50), default='dwarf')
+    gender = Column(Boolean, default=False)
+    dnd_class = Column(String(50), default='fighter')
+    hair = Column(String(50), default='black')
+    beard = Column(Boolean, default=False)
+    background = Column(String(50), default='forest')
 
-    triplets = relationship("Triplet", back_populates="user", order_by=Triplet.id)
+    current_face_image = Column(String(255), nullable=True)
+    current_target_image = Column(String(255), nullable=True)
+    current_result_image = Column(String(255), nullable=True)
 
 
 async def add_user(user_id, user_name, user_surname, user_nickname=None):
@@ -75,7 +51,6 @@ async def add_user(user_id, user_name, user_surname, user_nickname=None):
                 user_name=user_name,
                 user_surname=user_surname,
                 user_nickname=user_nickname)
-            existing_user.character = Character()
         session.add(existing_user)
         await session.commit()
         return existing_user
@@ -102,16 +77,32 @@ async def fetch_user_details(user_id):
     """Fetch specific attributes for a user by user_id."""
     async with async_session() as session:
         stmt = select(
-            User.dnd_class, User.race, User.beard, User.hair, User.background
-        ).where(User.id == user_id)
+            User.dnd_class, User.race, User.beard, User.hair, User.background,
+        User.current_face_image, User.current_target_image, User.current_result_image).where(User.id == user_id)
         result = await session.execute(stmt)
         user_details = result.first()
         if user_details:
-            print(user_details)
-            print(f"Class: {user_details.dnd_class}, Race: {user_details.race}, Beard: {user_details.beard}, "
+            classes = (f"Class: {user_details.dnd_class}, Race: {user_details.race}, Beard: {user_details.beard}, "
                   f"Hair: {user_details.hair}, Background: {user_details.background}")
+            triplets = (user_details.current_face_image, user_details.current_target_image,
+                        user_details.current_result_image)
+            return user_details
         else:
             print("User details not found.")
+
+async def update_attr(user_id, attribute_name, new_value):
+    async with async_session() as session:
+        user = await session.get(User, user_id)
+        if user:
+            if hasattr(user, attribute_name):
+                setattr(user, attribute_name, new_value)
+                await session.commit()
+                return True
+            else:
+                print(f"Attribute '{attribute_name}' not found on User.")
+                return False
+        print("User not found.")
+        return False
 
 
 async def create_tables():
